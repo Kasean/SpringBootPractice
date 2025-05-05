7dc9ec866513223ebb350ccb6a03d259218daedc
Siarhei_Kakichau <siarhei_kakichau@epam.com>
2025-05-05 13:33:35 +0300

    [First module]: init project

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Spring Boot Homework
 
 [tasks](Homework.md)
+
+### Setup:
+1. Java version - 21
+2. gradle version - 8.13
---
+++ src/test/java/com/kasean/spring/boot/homework/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasean.spring.boot.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
